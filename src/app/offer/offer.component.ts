352c4a33702c324fc6b1f1f790bd35efe94646ef
abhilash <abhilash.kamble376@gmail.com>
2020-11-05 22:47:40 +0530

    shop

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,17 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class OfferComponent implements OnInit {
 
 offers=[
-  {"offer_name":"Diwali Discount","shop_name":"Grace Motors","validity":"3/12/2020"},
+  {"id":1,"discount":20,"shop_name":"Grace Motors","coupon":"DIWALI20"},
+  {"id":2,"discount":40,"shop_name":"Bike Garage","coupon":"SUMMER12"},
+  {"id":3,"discount":10,"shop_name":"Dr Bike","coupon":"WINTER10"},
+  {"id":4,"discount":30,"shop_name":"Bike Master","coupon":"OFF20"},
+  {"id":5,"discount":25,"shop_name":"Bike Guru ","coupon":"SEASONEND30"},
+  {"id":6,"discount":5,"shop_name":"Clutch Motors","coupon":"MANSOON35"},
+
 
 ]
 
-  constructor() 
+  constructor(private router:Router) 
   {
 
   }
 
 
   ngOnInit(): void {
+  }
+
+  viewOffer(offer){
+   this.router.navigate(['/shop'],{queryParams:{id:offer['Vehicle_Number']}})
+
   }
 
 }
